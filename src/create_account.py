bfd3646806a0d22907006127f4d7f1b4a34fc626
jwlh00 <60154594+jwlh00@users.noreply.github.com>
2022-03-29 15:56:36 -0600

    Update create_account.py
    Creacion de cuenta con el hashing

--- src/create_account.py
+++ src/create_account.py
@@ -1,5 +1,5 @@
 import psycopg2
-
+import bcrypt
 
 x = False
 while not x:
@@ -9,26 +9,27 @@ while not x:
         password = str(input("Ingrese su contrasena\n"))
         state = "activo"
         
-        print(username)
-        print(email)
-        print(password)
+        #Hashing del password
+        pass_byte = bytes(password, 'utf-8')
+        hashed = bcrypt.hashpw(pass_byte, bcrypt.gensalt())
+        hashed = hashed.decode("utf-8")
+        
+
         
         conn = psycopg2.connect("host=localhost dbname=proyecto_2 user=postgres password=12345")
         cur = conn.cursor()
         cur.execute("INSERT INTO usuario (nombre_usuario, contraseña, correo, estado) values (%s, %s, %s, %s)",
-                    (username, password, email, state))
-
+                    (username, hashed, email, state))
                         
         x = True
     except Exception:
         print("\nEl usuario ya se encuentra registrado, intente de nuevo")
 
 
-
-
 conn.commit() #Commit de las tablas a base de datos SQL
 conn.close()  #Cerrar la conexion
 
   
+
 
 
