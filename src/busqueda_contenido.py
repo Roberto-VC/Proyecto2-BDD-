e72d27e319d0c7412d2cd3529f9239565bd917da
andresdlRoca <andresdelaroca11@gmail.com>
2022-04-04 16:50:58 -0600

    Implementacion de busqueda de contenido y visualizacion de multimedia

--- src/busqueda_contenido.py
+++ src/busqueda_contenido.py
@@ -1,8 +1,22 @@
+from cProfile import label
+from matplotlib import container
 import psycopg2
-import bcrypt
+import tkinter as tk
+from tkinter import OptionMenu, Scrollbar, StringVar, messagebox
+import tkinter.font as tkFont
+import vlc
+import pafy
+import time
+import keyboard
+
+from sympy import expand, false
 
 conn = psycopg2.connect("host=localhost dbname=proyecto_2 user=postgres password=rwby123")
 cur = conn.cursor()
+
+def clear_entradas(event, entry):
+    entry.delete(0, tk.END)
+    #view.pack_forget()
 
 def busqueda_actores(busqueda):
     
@@ -240,41 +254,138 @@ def busqueda_nombre(busqueda):
     WHERE nombre = 'Mision Imposible';
     """
 
-def main_busqueda():
-    print("Busqueda de contenido:\n")
-    print("Eliga una opcion valida para filtrar la busqueda: ")
-    print("1. Por nombre de actor")
-    print("2. Por nombre de director")
-    print("3. Por tipo de categoria (Genero)")
-    print("4. Por titpo de entretenimiento (Pelicula, serie, etc)")
-    print("5. Por nombre de premios ganados")
-    print("6. Por fecha de estreno (Año)")
-    print("7. Por nombre")
+def busqueda(searchWindow, inputusuario, busqueda):
 
-    inputusuario = int(input("Seleccione su opcion"))
+    listaBusqueda = []
 
-    if inputusuario == 1:
-        busqueda = input("Que actor busca?")
-        busqueda_actores(busqueda)
-    elif inputusuario == 2:
-        busqueda = input("Que director busca?")
-        busqueda_director(busqueda)
-    elif inputusuario == 3:
-        busqueda = input("Que categoria busca?")
-        busqueda_categoria(busqueda)
-    elif inputusuario == 4:
-        busqueda = input("Que tipo de entretenimiento busca?")
-        busqueda_entretenimiento(busqueda)
-    elif inputusuario == 5:
-        busqueda = input("Que premio ganado busca?")
-        busqueda_premios(busqueda)
-    elif inputusuario == 6:
-        busqueda=input("Que fecha de estreno busca?")
-        busqueda_estreno(busqueda)
-    elif inputusuario == 7:
-        busqueda = input("Que contenido busca?")
-        busqueda_nombre(busqueda)
+    if busqueda == "":
+        print("No se ha ingresado ningun valor")
+
+    if inputusuario == "Actor":
+        listaBusqueda = busqueda_actores(busqueda)
+    elif inputusuario == "Director":
+        listaBusqueda = busqueda_director(busqueda)
+    elif inputusuario == "Categoria":
+        listaBusqueda = busqueda_categoria(busqueda)
+    elif inputusuario == "Tipo":
+        listaBusqueda = busqueda_entretenimiento(busqueda)
+    elif inputusuario == "Premio ganado":
+        listaBusqueda = busqueda_premios(busqueda)
+    elif inputusuario == "Fecha de estreno":
+        listaBusqueda = busqueda_estreno(busqueda)
+    elif inputusuario == "Nombre":
+        listaBusqueda = busqueda_nombre(busqueda)
     else:
-        print("Opcion invalida seleccionada")
+        print("No se ingreso un valor valido")
+
+    count=0
+    if not listaBusqueda:
+        print("No se encontro ningun resultado :(")
+    else:
+        for widget in searchWindow.winfo_children():
+            widget.destroy()
+
+        for item in listaBusqueda:
+            labelTitulo = tk.Label(searchWindow, text=item[0], bg='#ffe4e1')
+            labelLink = tk.Button(searchWindow, text="Ver", bg='#ffe4e1', command=lambda x=item[1]:  visualizar(x)) #Temporal hasta reemplazar por boton
+            labelTitulo.grid(row=count, column=0, padx=100, pady=5)
+            labelLink.grid(row=count, column=1, padx=100)
+            count = count + 1
+        
+def visualizar(link):
+    print(link)
+    url=link
+    video = pafy.new(url)
+    best = video.getbest()
+    playurl = best.url
+    Instance = vlc.Instance()
+    player = Instance.media_player_new()
+    Media = Instance.media_new(playurl)
+    Media.get_mrl()
+    player.set_media(Media)
+    playing = True
+    player.play()
+    while True:
+        if keyboard.read_key() == "s" and playing == True:
+            player.pause()
+            playing = False
+        elif keyboard.read_key() == "p" and playing == False:
+            player.play()
+            playing == True
+        elif keyboard.read_key() == "e":
+            player.stop()
+            print("Supposed to exit here")
+            return False
+            
+
+def UI_busqueda():
+    background = '#ffe4e1'
+    foreground = '#79a1e0'
+    #pastWindow.destroy()
+
+    searchWindow = tk.Tk(className="Streameo (Working Title)")
+    entryarea = tk.Canvas(searchWindow, width=750, height=650, bg=foreground)
+    entryarea.place(relx=0.5, rely=0.5, anchor="center")
+    searchFont = tkFont.Font(family="@MS UI Gothic", size=8, weight="bold" )
+
+    #Render del logo de la aplicacion
+    logoCanvas = tk.Canvas(searchWindow, width = 150, height = 150, highlightthickness=0, bg=foreground)
+    logo = tk.PhotoImage(file="src/assets/streameologo.png")
+    logoCanvas.create_image(130,10, anchor="ne", image=logo)
+    logoCanvas.place(relx=0.49, rely=0.15, anchor="center")
+    containerBusqueda = tk.Frame(searchWindow)
     
-main_busqueda()
+    resultadosBusqueda = tk.Canvas(containerBusqueda, width=600, height=300, bg=background)
+    #resultadosBusqueda.pack()
+    resultadosBusqueda.grid_propagate(false)
+    resultadosBusqueda.pack_propagate(false)
+    scrollbar = tk.Scrollbar(containerBusqueda, orient="vertical", command=resultadosBusqueda.yview)
+    scrollable_frame = tk.Frame(resultadosBusqueda)
+    scrollable_frame.configure(bg=background)
+    scrollable_frame.bind(
+        "<Configure>",
+        lambda e: resultadosBusqueda.configure(
+            scrollregion=resultadosBusqueda.bbox("all")
+        )
+    )
+    resultadosBusqueda.create_window((0,0), window=scrollable_frame, anchor="nw")
+    resultadosBusqueda.configure(yscrollcommand=scrollbar.set)
+
+    containerBusqueda.place(relx=0.5, rely=0.7, anchor="center")
+    resultadosBusqueda.pack(side="left", fill="both", expand=True)
+    scrollbar.pack(side="right", fill="y")
+
+    inputBusqueda = tk.Entry(entryarea, width=90)
+    inputBusqueda.configure(bg=background)
+    inputBusqueda.insert(0, "Busqueda...")
+    inputBusqueda.bind("<Button-1>", lambda event: clear_entradas(event, inputBusqueda))
+    inputBusqueda.place(relx=0.5, rely=0.28, anchor="center")
+
+    opcionesBusqueda = ["Actor", "Director", "Categoria", "Tipo", "Premio ganado", "Fecha de estreno", "Nombre"]
+    clicked = StringVar()
+    clicked.set("Nombre")
+    opcionesMenu = OptionMenu(searchWindow, clicked, *opcionesBusqueda)
+    opcionesMenu.configure(bg=background)
+    opcionesMenu.place(relx=0.4, rely=0.35, anchor="center")
+    buscar = tk.Button(searchWindow, bg=background, width=8, height=3, text="Buscar", font=searchFont, command=lambda: busqueda(scrollable_frame, clicked.get(), inputBusqueda.get()))
+    buscar.place(relx = 0.6, rely=0.35, anchor="center")
+
+    favoritos = tk.Button(searchWindow, bg=background, width=15, height=3, text="Favoritos", font=searchFont)
+    historial = tk.Button(searchWindow, bg=background, width=15, height=3, text="Historial", font=searchFont)
+    recomendaciones = tk.Button(searchWindow, bg=background, width=15, height=3, text="Recomendaciones", font=searchFont)
+    volver = tk.Button(searchWindow, bg=background, width=8, height=3, text="Volver", font=searchFont)
+    favoritos.place(relx=0.3, rely=0.4)
+    historial.place(relx=0.45, rely=0.4)
+    recomendaciones.place(relx=0.6, rely=0.4)
+    volver.place(relx=0.15, rely=0.05)
+
+
+
+    #Configuraciones extra de ventana
+    searchWindow.configure(bg=background)
+    searchWindow.geometry("1000x700")
+    searchWindow.resizable(False,False)
+    searchWindow.mainloop()
+
+UI_busqueda()
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from create_account import *
 from login import *
-from busqueda_contenido import *
+#from busqueda_contenido import *
 from perfiles import *
 import tkinter as tk
 from tkinter import messagebox
@@ -88,31 +88,3 @@ window.geometry("900x500")
 window.resizable(False,False)
 window.mainloop()
 
-
-
-def main():
-    run = True
-    while run: 
-        print("---Bienvenido a Streameo (Working title)---")
-        print("--Seleccione una opcion--\n")
-        print("1. Login")
-        print("2. Signup")
-        print("3. Salir\n")
-
-        inputmenuprincipal = input("\nOpcion: ")
-        if inputmenuprincipal == "1":
-            inputusuario = input("Ingrese su usuario: ")
-            inputcontraseña = input("Ingrese su contraseña: ")
-            if loginInfo(inputusuario, inputcontraseña):
-                print("\nSe ha logrado loguear al sistema\n")
-                print("--Menu de selección de perfiles--")
-                perfiles(inputusuario)
-            else:
-                print("\nNo se ha logrado loguear al sistema\n")
-        elif inputmenuprincipal == "2":
-            signup()
-        elif inputmenuprincipal == "3":
-            print("\nSaliendo del programa...")
-            run = False
-        else:
-            print("\nIngreso una opcion invalida\n")
