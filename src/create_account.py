74e2c1b6e20e88ba7634a09c915fea5b8ce9d387
andresdlRoca <andresdelaroca11@gmail.com>
2022-04-15 13:04:31 -0600

    Revision de caracteres especiales

--- src/create_account.py
+++ src/create_account.py
@@ -1,36 +1,46 @@
 import psycopg2
 import bcrypt
+import re
+
+disallowedchars="!#$%&/()='-|;"
 
 def signup():
     x = False
     while not x:
-        try:
-            username = str(input("Ingrese su usuario\n"))
-            email = str(input("Ingrese su correo\n"))
-            password = str(input("Ingrese su contrasena\n"))
-            state = "activo"
-            
-            #Hashing del password
-            pass_byte = bytes(password, 'utf-8')
-            hashed = bcrypt.hashpw(pass_byte, bcrypt.gensalt(10))
-            hashed = hashed.decode("utf-8")
-            
-
-            
-            conn = psycopg2.connect("host=localhost dbname=proyecto_2 user=postgres password=rwby123")
-            cur = conn.cursor()
-            cur.execute("INSERT INTO usuario (nombre_usuario, contraseña, correo, estado) values (%s, %s, %s, %s)",
-                        (username, hashed, email, state))
-                            
+        username = str(input("Ingrese su usuario\n"))
+        if set(username) & set(disallowedchars):
+            print("No se permiten caracteres especiales")
             x = True
-        except Exception:
-            print("\nEl usuario ya se encuentra registrado, intente de nuevo")
+        email = str(input("Ingrese su correo\n"))
+        if set(email) & set(disallowedchars):
+            print("No se permiten caracteres especiales")
+            x = True
+        password = str(input("Ingrese su contrasena\n"))
+        if set(password) & set(disallowedchars):
+            print("No se permiten caracteres especiales")
+            x = True
+        state = "activo"
+        
+        #Hashing del password
+        pass_byte = bytes(password, 'utf-8')
+        hashed = bcrypt.hashpw(pass_byte, bcrypt.gensalt(10))
+        hashed = hashed.decode("utf-8")
+        
+
+        
+        conn = psycopg2.connect("host=localhost dbname=proyecto_2 user=postgres password=rwby123")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO usuario (nombre_usuario, contraseña, correo, estado) values (%s, %s, %s, %s)",
+                    (username, hashed, email, state))
+                        
+        x = True
+
 
 
     conn.commit() #Commit de las tablas a base de datos SQL
     conn.close()  #Cerrar la conexion
 
-  
+signup()
 
 
 
